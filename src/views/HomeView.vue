9856224c5a546b3989aec7308b5367c98e964e9c
MatheoDev <matheo.deknuydt@gmail.com>
2023-11-08 15:41:35 +0100

    axios conf

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+// import { onMounted } from 'vue'
+
+// import { injectStrict } from '@/utils/injectTyped'
+// import { AxiosKey } from '@/conf/axios'
+
+// const axios = injectStrict(AxiosKey)
+
+// onMounted(async () => {
+//   const { data } = await axios.get('/supercharger')
+//   console.log(data)
+// })
+
 </script>
 
 <template>
