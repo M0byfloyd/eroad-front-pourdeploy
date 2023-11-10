418f08c7b114f530ecaaafc2aca56521f0a7bc5e
Alexandre Robert <54885010+M0byfloyd@users.noreply.github.com>
2023-11-10 11:21:40 +0100

    Update axios.ts

--- src/conf/axios.ts
+++ src/conf/axios.ts
@@ -2,7 +2,7 @@ import axios, { type AxiosInstance } from 'axios'
 import type { InjectionKey } from 'vue'
 
 const instance: AxiosInstance = axios.create({
-  baseURL: 'https://eroad-back.netlify.app/',
+  baseURL: 'https://eroad-back.netlify.app:3000/',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json'
